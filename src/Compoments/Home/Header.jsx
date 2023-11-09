e34abc8b54723e5afcfdace127bd39122c8d75ee
mayurOpticalarc.com <mayurkukade@opticalarc.com>
2023-11-09 12:39:37 +0530

    update

--- src/Compoments/Home/Header.jsx
+++ src/Compoments/Home/Header.jsx
@@ -3,39 +3,42 @@ import preferable from "../Images/preferable2.png";
 
 export default function Header() {
   return (
-    <div className="flex bg-background container mx-auto " >
-
+    <div className="flex bg-background container mx-auto ">
       {/* left side of container */}
-      <div className="md:pt-20 md:mb-8 pt-20 md:spl-[50px]  ">
-        <p className=" text-6xl font-[Poppins] font-semibold text-text ">
+      <div className="md:pt-20 md:mb-8  md:spl-[50px]  ">
+        <p className=" md:text-6xl font-[Poppins] font-semibold text-text xl:text-8xl ">
           We Help
-          
           <div className="pt-3">
-          <span className="text-accent  ">You</span> Get The</div>
+            <span className="text-accent   ">You</span> Get The
+          </div>
           <div className="pt-3">
-          Right <span className="text-accent ">Job</span></div>
+            Right <span className="text-accent ">Job</span>
+          </div>
         </p>
 
-        <p className="mt-10 font-[Poppins] text-2xl">Join us and take your career to the <br /> next level.</p>
+        <p className="mt-10 font-[Poppins] md:text-2xl xl:text-2lg">
+          Join us and take your career to the <br /> next level.
+        </p>
 
-          <div className="flex mt-10"> 
-          <button className="  font-[Poppins] w-44 h-12  mt-3 text-lg font-bold bg-secondary rounded-md">How does it work?</button>           
-        <button className="bg-primary text-white md:ml-10 ml-5 content-center font-bold w-44 h-12 rounded-md mt-3">
-          Get Started
-        </button>
-        </div> 
+        <div className="flex md:mt-10 ">
+          <button className="  font-[Poppins] md:w-44 md:h-12 xl:w-[12rem]  md:mt-3 md:text-lg xl:text-xl  font-bold bg-secondary rounded-md">
+            How does it work?
+          </button>
+          <button className="bg-primary text-white md:ml-10 ml-5 content-center xl:text-xl font-bold w-44 h-12 rounded-md mt-3">
+            Get Started
+          </button>
+        </div>
       </div>
 
       {/* Right side of container */}
 
       <div className="  ">
         <img
-          className="md:h-[32.5rem] xl:h-[45.375rem] md:w-[41.875rem] md:right-0   xl:w-[60rem] absolute  "
+          className="md:h-[32.5rem]  md:w-[41.875rem] md:right-0   xl:w-[60rem] xl:h-[46.375rem] absolute  "
           src={preferable}
           alt="image"
         />
       </div>
-      
     </div>
   );
 }
--- src/Compoments/Home/HeaderLower.jsx
+++ src/Compoments/Home/HeaderLower.jsx
@@ -1,35 +1,77 @@
 import React from "react";
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { useEffect } from "react";
 
 export default function HeaderLower() {
+  const count1 = useMotionValue(0);
+  const count2 = useMotionValue(0);
+  const count3 = useMotionValue(0);
+  const count4 = useMotionValue(0);
+
+  // Create rounded values based on count1 and count2
+  const rounded1 = useTransform(count1, Math.round);
+  const rounded2 = useTransform(count2, Math.round);
+  const rounded3 = useTransform(count3, Math.round);
+  const rounded4 = useTransform(count4, Math.round);
+
+  useEffect(() => {
+    // First animation to 400
+    const animation1 = animate(count1, 1200, {
+      duration: 2
+    });
+
+    // Second animation to 300
+    const animation2 = animate(count2, 200, {
+      duration: 2
+    });
+    // First animation to 400
+    const animation3 = animate(count3, 2000, {
+      duration: 2
+    });
+
+    // Second animation to 300
+    const animation4 = animate(count4, 30, {
+      duration: 2
+    });
+
+    // Stop both animations when the component unmounts
+    return () => {
+      animation1.stop();
+      animation2.stop();
+      animation3.stop();
+      animation4.stop();
+    };
+  }, []);
   return (
     <>
-      <div className="w-full mt-6  h-3 flex-shrink-0 bg-primary "></div>
+    
+      <div className="w-full md:mt-3 xl:mt-[7rem]   h-3 flex-shrink-0 bg-primary "></div>
       <div className="flex justify-center bg-faintBackgroundBlue p-5  ">
-        <div className="md:grid md:grid-cols-12 mt-5  md:divide-x-4 md:divide-gray-900">
+        <div className="md:grid md:grid-cols-12 mt-5 xl:gap-12 md:divide-x-4 md:divide-gray-900">
           {/* First block */}
           <div className="col-span-3 w-[300px]  ">
-            <p className="text-3xl text-accent flex justify-center">1200+</p>
+            <p className="text-3xl text-accent flex justify-center">  <motion.h1>{rounded1}</motion.h1>+</p>
             <p className="text-[30px] flex justify-center text-[#060000]">
               Job Available
             </p>
           </div>
           {/* Second block */}
           <div className="col-span-3 w-[300px]  ">
-            <p className="text-3xl text-accent flex justify-center">200+</p>
+            <p className="text-3xl text-accent flex justify-center"><motion.h1>{rounded2}</motion.h1>+</p>
             <p className="text-[30px] flex justify-center text-[#060000]">
               Top Compimes
             </p>
           </div>
           {/* First block */}
           <div className="col-span-3 w-[300px]  ">
-            <p className="text-3xl text-accent flex justify-center">2000+</p>
+            <p className="text-3xl text-accent flex justify-center"><motion.h1>{rounded3}</motion.h1>+</p>
             <p className="text-[30px] flex justify-center text-[#060000]">
               Canditate Hired
             </p>
           </div>
           {/* First block */}
           <div className="col-span-3 w-[300px]  ">
-            <p className="text-3xl text-accent flex justify-center">30+</p>
+            <p className="text-3xl text-accent flex justify-center"><motion.h1>{rounded4}</motion.h1>+</p>
             <p className="text-[30px] flex justify-center text-[#060000]">
               Class Available
             </p>
