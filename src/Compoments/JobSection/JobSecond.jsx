2d19ec7afae549a618884296e00ca885f7e454d3
mayurkukade <mayurkukade@opticalarc.com>
2023-11-20 10:27:01 +0530

    check

--- src/Compoments/JobSection/JobSecond.jsx
+++ src/Compoments/JobSection/JobSecond.jsx
@@ -173,7 +173,7 @@ export default function JobSecond() {
                 color="blue-gray"
                 className="text-[#5594d5]"
               >
-                Full Stack Web Developer
+                Full Stack Web Developers
               </Typography>
               <Typography variant="h6" color="gray" className=" uppercase">
                 lenskart
