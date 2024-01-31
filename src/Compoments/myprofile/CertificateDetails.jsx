5150c28e8a8690cd172aa359364aef2720904fa6
mayurkukade <mayurkukade@opticalarc.com>
2024-01-31 09:45:54 +0530

    update

--- src/Compoments/myprofile/CertificateDetails.jsx
+++ src/Compoments/myprofile/CertificateDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaEdit } from "react-icons/fa";
 import {
   Button,
---
+++ src/Compoments/myprofile/EducationDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EducationDetails = () => {
+  return (
+    <>
+    <p>Education</p>
+      
+    </>
+  )
+}
+
+export default EducationDetails
